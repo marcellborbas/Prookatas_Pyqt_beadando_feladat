ce9635d549e44802ea8a8939297d5dbc5bf5b336
marcellborbas <borbas.marcell@freemail.hu>
2025-08-02 17:16:25 +0200

    Kölcsönzött könyvek megjelenítése lapozással

---
+++ modules/my_loans_window.py
@@ -0,0 +1,43 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QHeaderView, QHBoxLayout, QPushButton, QLabel
+
+class MyLoansWindow(QWidget):
+    def __init__(self, db, user_id):
+        super().__init__()
+        self.setWindowTitle("Kikölcsönzött könyveim")
+        self.db = db
+        self.user_id = user_id
+
+        self.loans = self.get_loans()           # <--- Most elmentjük egy listába
+        self.rows_per_page = 10                 # <--- Hány sor legyen egy oldalon
+        self.current_page = 0                   # <--- Aktuális oldal
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(10, 10, 10, 10)
+
+        # Táblázat létrehozása a kölcsönzött könyvek adatainak megjelenítésére
+        self.table = QTableWidget()
+        self.table.setColumnCount(7)
+        self.table.setHorizontalHeaderLabels([
+            "Cím", "Szerző(k)", "ISBN", "Kölcsönzés dátuma", "Lejárat", "Visszahozva", "Státusz"
+        ])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.setSizeAdjustPolicy(QTableWidget.SizeAdjustPolicy.AdjustToContents)
+        layout.addWidget(self.table)
+
+        # Lapozó gombok és címke
+        pagelayout = QHBoxLayout()
+        self.prev_btn = QPushButton("Előző")
+        self.prev_btn.clicked.connect(self.show_prev_page)
+        self.next_btn = QPushButton("Következő")
+        self.next_btn.clicked.connect(self.show_next_page)
+        self.page_label = QLabel()
+        pagelayout.addWidget(self.prev_btn)
+        pagelayout.addWidget(self.page_label)
+        pagelayout.addWidget(self.next_btn)
+        layout.addLayout(pagelayout)
+
+        self.setLayout(layout)
+        self.resize(1200, 450)
+        self.update_table()
+
